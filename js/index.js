03b38c7767c639e50603dcceea0ccf7b934bec0e
Vladimir <79854322262@yandex.ru>
2019-10-15 20:41:53 +0300

    Попытка 2 уменьшить index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-
-
     const buttonsContainer = document.querySelector('.buttons-wrap');
-
-    buttonsContainer.innerHTML = '<button class="button button_yellow" type="button">Да</button>' +
-        '<button class="button">Нет</button>';
-
     const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
     const confirmPurchaseButton = document.querySelector(".buttons-wrap .button_yellow");
     const purchaseListContainer = document.createElement('div');
@@ -34,11 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
         buttonsContainer.style.display = "none";
     }
 
-
-
     document.getElementsByClassName("header-menu__switcher")[0].addEventListener("click", function() {
         document.getElementsByClassName("header-menu")[0].classList.toggle("header-menu_active")
     });
-
-
-}, !1);
+}, 1);
